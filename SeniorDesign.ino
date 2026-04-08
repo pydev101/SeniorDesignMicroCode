4e3bf9af48e0c9fc48b9111fb1cbd4fea169af36
Carson E <30803947+pydev101@users.noreply.github.com>
2026-04-07 20:22:25 -0500

    changes

--- SeniorDesign.ino
+++ SeniorDesign.ino
@@ -8,7 +8,7 @@
 #include <cmath>
 #include <cstdint>
 #include <atomic>
-
+#include "hardware/timer.h"
 
 // --- Configuration -----------------------------------------------------
 
@@ -26,6 +26,10 @@
 #define DMUX_S3 2
 #define DMUX_S4 1
 #define DMUX_S5 0
+
+const uint8_t DMUXIN_PINS[] = {DMUX_S0, DMUX_S1, DMUX_S2, DMUX_S3, DMUX_S4, DMUX_S5};
+uint32_t DMUXIN_MASKS[6];
+
 #define AMUX_S0 19
 #define AMUX_S1 20
 #define AMUX_S2 21
@@ -117,6 +121,7 @@ public:
     void getSample() {
         if (mode == 0) return;
 
+        // TODO Linear amp function may sound clunky could look into expoential functions
         for (int i = 0; i < GENERATION_SAMPLES; i++) {
             if (mode == 1) { // Attack
                 amp += ASlope;
@@ -134,7 +139,7 @@ public:
                 }
             }
 
-            sampleValues[i] += amp * sinf(phase); // TODO Sine lookup table or taylor series if pure sine function is too slow
+            sampleValues[i] += amp * sinf(phase); // TODO Sine lookup table or parabolic approx if pure sine function is too slow when several notes play
 
             phase += phaseStep;
             if (phase >= PI) phase -= TWOPI; // Bound between -PI and PI
@@ -142,79 +147,125 @@ public:
     }
 };
 
-Note myNote(440.0f, 3.0f, 1.0f, 0.4f, 3.0f);
-Note myNote2(500.0f, 1.5f, 0.8f, 0.5f, 1.0f);
+int getKey(int key) {
+    int idx = key - 1; // 0-28
+    int reg = 0;
+
+    if (idx < 24) {
+        reg = idx;
+    } else {
+        reg = (idx - 21) << 3;
+    }
+
+    // Direct Register Write Loop
+    for (int b = 0; b < 6; b++) {
+        if ((reg >> b) & 1) {
+            sio_hw->gpio_set = DMUXIN_MASKS[b]; // Atomic SET
+        } else {
+            sio_hw->gpio_clr = DMUXIN_MASKS[b]; // Atomic CLEAR
+        }
+    }
+
+    delayMicroseconds(5); // Minimum time for electrical settling
+    
+    // Fast Read: Check if the DMUX_OUT pin bit is set in the input register
+    return (sio_hw->gpio_in & (1ul << DMUX_OUT)) ? 1 : 0;
+}
+
+std::atomic<unsigned long int> SHARED_inputs; //32 bits
+
+// Humans feel lag in >15mS
+const int scanPeriod = 5000; //uS or slower (Must be slower than the funtion, but faster than at least 5mS)
+const int rawInputLen = 3;
+unsigned long int lastRAWInputs[rawInputLen];
+unsigned int lawRAWInputsIndex = 0;
+
+bool scanInputs(struct repeating_timer *t){
+  // Key Press Scanning
+  unsigned long int inputs = 0;
+  for(int i=0; i<29; i++){
+    inputs |= getKey(i)<<i;
+  }
+
+  // Debounce logic (Pins are pulled low by default thus any high would start immediate attack; Any high pins will stay high creating debounce logic prevening premature release)
+  lastRAWInputs[lawRAWInputsIndex] = inputs;
+  lawRAWInputsIndex = (lawRAWInputsIndex + 1) % rawInputLen; // Circular buffer for inputs
+
+  inputs = 0;
+  for(int i=0; i<rawInputLen; i++){
+      inputs |= lastRAWInputs[i]; // Keep any high bits high (immedate attack); only once all previous samples of the bit is clear then the key is released (debounce)
+  }
+  
+  // Send inputs to digital synth
+  SHARED_inputs.store(inputs, std::memory_order_release);
+
+  
+  // Pent Scanning
+  // TODO, need filter (EMA?), minimum steps?
+  // TODO Add Update Flag
+
+  return true;
+}
+
+struct repeating_timer scanTimer;
 
 // Setup CORE 0
 void setup() {
   Serial.begin(115200);
-  
-  i2s.setBitsPerSample(16); // TODO Move to core 1
-  i2s.begin(SAMPLE_RATE);
 
   // Digital In
   pinMode(DMUX_OUT, INPUT);
-  pinMode(DMUX_OUT, INPUT_PULLDOWN); // TODO Pullup or pull down?
+  pinMode(DMUX_OUT, INPUT_PULLDOWN);
   pinMode(OCT_DOWN_P, INPUT);
   pinMode(OCT_DOWN_P, INPUT_PULLDOWN);
   pinMode(OCT_UP_P, INPUT);
   pinMode(OCT_UP_P, INPUT_PULLDOWN);
-  
-  pinMode(DMUX_S0, OUTPUT);
-  pinMode(DMUX_S1, OUTPUT);
-  pinMode(DMUX_S2, OUTPUT);
-  pinMode(DMUX_S3, OUTPUT);
-  pinMode(DMUX_S4, OUTPUT);
-  pinMode(DMUX_S5, OUTPUT);
-  
+
+  // Digital Out
+  for(int i = 0; i < 6; i++) {
+    gpio_init(DMUXIN_PINS[i]);
+    gpio_set_dir(DMUXIN_PINS[i], GPIO_OUT);
+    DMUXIN_MASKS[i] = (1ul << DMUXIN_PINS[i]); // Pre-calculate the bitmask
+  }
+
   pinMode(AMUX_S0, OUTPUT);
   pinMode(AMUX_S1, OUTPUT);
   pinMode(AMUX_S2, OUTPUT);
 
+  // Analog IN
   pinMode(AMUX_OUT, INPUT);
   pinMode(AUDIO_IN, INPUT);
-}
 
-std::atomic<unsigned long int> x; //32 bits
-void scanInputs(){
-  // Key Press Scanning
-
-  // Pent Scanning
-  
+  add_repeating_timer_us(-scanPeriod, scanInputs, NULL, &scanTimer);
 }
 
 // Loop CORE 0
 void loop() {
-  // Input Scanning
-  
-  
-    // Process Note
-    while(Serial.available() > 0){
-        char x = Serial.read();
-        Serial.println(x);
-        if(x=='a'){
-            myNote.updateInput(true);
-        }else if(x=='b'){
-            myNote.updateInput(false);
-        }
+  // TODO Update Screen
+}
 
-        if(x=='c'){
-            myNote2.updateInput(true);
-        }else if(x=='d'){
-            myNote2.updateInput(false);
-        }
-    }
+/// CORE 1 -------------------------------------------------
+// Digital Syth Core
 
-  
+// Setup CORE 1
+void setup1() {
+  i2s.setBitsPerSample(16);
+  i2s.begin(SAMPLE_RATE);
+}
+
+Note myNote(440.0f, 3.0f, 1.0f, 0.4f, 3.0f); // TODO Implement all notes
+Note myNote2(500.0f, 1.5f, 0.8f, 0.5f, 1.0f);
+
+// Loop CORE 1
+void loop1() {
   // Clear buffer
   for (int i = 0; i < GENERATION_SAMPLES; i++){
       sampleValues[i] = 0;
       samples[i] = 0;
   }
-
-  myNote.getSample();
+  
+  myNote.getSample(); // TODO Implement thread safe input (copy data to local var then distribute)
   myNote2.getSample();
-
   
   for (int i = 0; i < GENERATION_SAMPLES; i++) {
       float v = sampleValues[i];
@@ -228,19 +279,4 @@ void loop() {
       i2s.write(samples[i]);
       i2s.write(samples[i]);
   }
-
-}
-
-/// CORE 1 -------------------------------------------------
-// Digital Syth Core
-
-// Setup CORE 1
-void setup1() {
-  // Initialize anything specific to Core 1 here
-}
-
-// Loop CORE 1
-void loop1() {
-  // Simulate a complex task
-  delay(1000); 
 }
